976d0976191ffb527cd07ae4729a56dd23b7ae41
Wunder9l <vvarty7@gmail.com>
2021-05-08 22:46:40 +0300

    Small clean up

--- ml_project/src/data/make_dataset.py
+++ ml_project/src/data/make_dataset.py
@@ -156,17 +156,3 @@ def prepare_data(args: PrepareDataArgs, train_output, test_output, vocab_output)
         test_df, label_encoder, transforms=None, tokenizer=tokenizer, vocab=vocab
     )
     save_dataset(test_ds, test_output)
-
-
-if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    # load_dotenv(find_dotenv())
-
-    prepare_data()
